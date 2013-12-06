3a5497ab90c40c7d7514084b45f86b6e288fe261
Ben Briggs <therealbenbriggs@hotmail.com>
2013-12-06 23:39:36 +0000

    Now returns a buffer.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function(){
     // Use csso(true) to turn structure minimization off.
     var optimise = (arguments.length > 0) ? arguments[0] : false;
     return es.map(function(file, cb) {
-        file.contents = csso.justDoIt(String(file.contents), optimise);
+        file.contents = new Buffer(csso.justDoIt(String(file.contents), optimise));
         cb(null, file);
     });
 };
